1ba3d392f5259c0f68ff1354f40307cc0dfbf0fe
Zain Ul Abdeen <zu4425@gmail.com>
2026-01-27 22:09:25 +0500

    lecture one add 55:38

---
+++ projectpractice/projectpractice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace projectpractice
+{
+    class Program
+    {
+        // 'out' is used to pass a variable to a method
+        // so the method can assign a value to it
+        // and send that value back to the caller
+
+
+        void userinput(out int a ,out  int b)
+        {
+            Console.WriteLine("Enter First number:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter  second  number:");
+             b= Convert.ToInt32(Console.ReadLine());
+        }
+        void sum(int a,int b)
+        {
+            
+            Console.WriteLine(a + b);
+        }
+
+        void sub(int a , int b)
+        {
+           
+            Console.WriteLine(a - b);
+        }
+
+        static void Main(string[] args)
+        {
+            
+            Program obj = new Program();
+            int num1, num2;
+            obj.userinput(out num1,out num2);
+            obj.sum(num1, num2);
+            obj.sub(num1, num2);
+        }
+    }
+}
